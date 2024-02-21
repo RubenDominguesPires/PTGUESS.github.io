06005b6558207a5b8868ceecbcfe76d9e248f88e
pires <rubendominguespires@gmail.com>
2024-02-21 21:32:26 +0000

    JavaScript Changes
    Adicionado !!! - Código para fazer aparecer o form quando for clicado em "Jogar";

---
+++ JavaScript.js
@@ -0,0 +1,6 @@
+var botao = document.getElementById("PlayButton");
+var formulario = document.getElementById("Formulario");
+
+botao.addEventListener("click", function () {
+  formulario.style.display = "initial";
+});
