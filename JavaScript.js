2732d43acc57fb874ad32a7b006bed4e254f1603
pires <rubendominguespires@gmail.com>
2024-03-12 18:55:12 +0000

    JavaScript Changes (TIMEDISPLAY TENTATIVE)
    Adicionado (var timedisplay) ✅ -- Criei esta variável que vai buscar ao documento html um 'ID' OU 'CLASS' que tenha o nome de ".TimeDisplay';
    
    Adicionado (TimeDisplay delay code) ✅ -- Desenvolvi uma função para aplicar um atraso na função "timeDisplay". Esta função consiste em criar um temporizador dentro da função mencionada. Quando este temporizador termina, altera o estilo de exibição do ".TimeDisplay" de 'none' (já definido no CSS) para 'flex', tornando assim o ".TimeDisplay", que estava invisível, visível. No entanto, este processo só ocorre após um intervalo de 1100 milissegundos, equivalente a 1.1 segundos, criando assim um atraso.

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,12 @@
 var botao = document.getElementById("PlayButton");
 var formulario = document.getElementById("Formulario");
+var timeDisplay = document.querySelector(".TimeDisplay");
 
 botao.addEventListener("click", function () {
   formulario.style.display = "initial";
+
+  setTimeout(function () {
+    timeDisplay.style.display = "flex";
+  }, 1100);
+
 });
