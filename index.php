57d91aa65db543b69b78882457e634f1a7e8eb53
Rúben Domingues Pires <rubendominguespires@gmail.com>
2024-07-12 12:39:44 +0100

    GRANDE ATUALIZAÇÃO (Index.php)
    ✅ Adicionado - Ícones do Bootstrap:
    
    Foi incorporada uma biblioteca de ícones do Bootstrap para facilitar o uso de ícones no site.
    ✅ Adicionado - Organização por Classes de Row e Column:
    
    O código foi organizado utilizando classes de row e column, o que simplifica a organização e torna o site mais responsivo.
    ❌ & 🔁 Remoção e Substituição do Ícone de Hambúrguer:
    
    O ícone de hambúrguer do Bootstrap, que estava em comentário, foi removido. Em seu lugar, foi adicionado um ícone de hambúrguer animado conforme um tutorial. Este ícone animado é clicável e abre um menu lateral. Esse menu permite ao usuário escolher a dificuldade do jogo, o tema do site (claro ou escuro), enviar sugestões (ainda a ser implementado) e acessar informações sobre "nós".
    ✅ Adicionado - Menu Interativo:
    
    Foi introduzido um menu interativo que oferece diversas opções ao usuário. Inclui seleção de dificuldade (fácil, médio, difícil), com botões semelhantes ao botão "JOGAR", sendo que a dificuldade fácil é a predefinida. Para o tema, foram adicionados ícones indicativos do tema escuro (lua). Posteriormente, funções JavaScript serão implementadas para permitir ao usuário alternar entre temas, trocando o ícone da lua por um sol.
    ✅ Adicionado - Align-content:
    
    Foram aplicados ajustes de align-content em dois elementos TimeBox. No minuto, foi utilizado flex-end para aproximá-lo da caixa central, enquanto no milisegundo, foi utilizado flex-start para posicioná-lo mais próximo ao centro. Essas alterações foram feitas para centralizar melhor esses elementos na página.

--- index.php
+++ index.php
@@ -5,70 +5,114 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>PTGUESS</title>
-  <link rel="stylesheet" href="css\styles.css" />
-  <link rel="shortcut icon" href="Imagens\favicon.png" type="image/x-icon" />
+  <link rel="stylesheet" href="css/styles.css" />
+  <link rel="shortcut icon" href="Imagens/favicon.png" type="image/x-icon" />
+  <!-- Option 1: Include in HTML -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 </head>
 
 <body>
-  <!--CAIXA-->
   <div class="caixa">
-    <!--TITULO-->
-    <div class="Titulo">
-      <!--LIST BUTTON-->
-      <!-- <svg
-          id="ListButton"
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-list"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m
-             0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-          />
-        </svg> -->
-      <!--/LIST BUTTON-->
-      <!--TITULO (h1) PTGUESS-->
-      <h1>PTGUESS</h1>
-      <!--/TITULO (h1) PTGUESS-->
-    </div>
-    <!--/TITULO-->
-    <div class="SubTitulo">
-      <!-- SUBTITULO-->
-      <p>Encontra a palavra antes que o tempo termine!</p>
-      <p>Quanto mais rapido adivinhares mais pontos ganhas!</p>
-      <!-- /SUBTITULO-->
-    </div>
-    <div class="PlayButton">
-      <!--PLAY BUTTON-->
-      <button type="button" id="PlayButton">JOGAR</button>
-      <!--/PLAY BUTTON-->
-    </div>
-    <div class="Formulario">
-      <form action="" autocomplete="on" style="width: 100%; height: 100%">
-        <input type="text" name="palavra" id="Formulario" placeholder="Insira a palavra" />
-      </form>
-    </div>
-    <div class="TimeDisplay">
-      <div class="Time-Box">
-        <h5>MINUTOS</h5>
-        <h1 id="minuto">02</h1>
+    <img id="confetti" src="./Imagens/648695_03eca.gif" alt="Confetti">
+    <div class="title-row">
+      <div class="Title-column">
+        <div class="Titulo">
+          <h1>PTGUESS</h1>
+        </div>
       </div>
-      <div class="Time-Box">
-        <h5>SEGUNDOS</h5>
-        <h1 id="segundo">30</h1>
+      <div class="Hamburguer-column">
+        <div class=" off-screen-menu">
+          <div class="side-menu-row">
+            <ul>
+              <div class="row">
+                <details>
+                  <summary style="cursor:pointer">Dificuldade</summary>
+                  <div class="difficulty-row">
+                    <div class="difficulty-column">
+                      <button class="difficulty-button active" id="botao-facil">Fácil</button>
+                    </div>
+                    <div class="difficulty-column">
+                      <button class="difficulty-button" id="botao-medio">Medio</button>
+                    </div>
+                    <div class="difficulty-column">
+                      <button class="difficulty-button" id="botao-dificil">Dificil</button>
+                    </div>
+                  </div>
+                </details>
+              </div>
+              <div class="row">
+                <a>Tema: <i class="bi-moon" id="toggleLight"></i></a><br />
+              </div>
+              <div class="row">
+                <a>Sobre Nós</a><br />
+              </div>
+              <div class="row">
+                <a>Sugestôes</a>
+              </div>
+            </ul>
+          </div>
+        </div>
+        <nav>
+          <div class="ham-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </nav>
       </div>
-      <div class="Time-Box">
-        <h5>MILISEGUNDOS</h5>
-        <h1 id="milisegundo">99</h1>
+    </div>
+    <div class="row">
+      <div class="SubTitulo">
+        <p class="subtituloText">Encontra a palavra antes que o tempo termine!</p>
+        <p class="subtituloText">Quanto mais rápido adivinhares mais pontos ganhas!</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="PlayButton">
+        <form action="" method="post">
+          <button type="button" id="PlayButton">JOGAR</button>
+        </form>
+      </div>
+    </div>
+    <div class="row" style="text-align:center">
+      <div class="Formulario">
+        <form action="" autocomplete="on">
+          <input type="text" autocomplete="off" name="palavra" id="Formulario" placeholder="Insira a palavra" disabled />
+        </form>
+      </div>
+    </div>
+    <div class="Image-row">
+      <div class="ImageBox">
+        <h2 id="tema" class="TemaBox"></h2>
+        <img id="imagem" draggable="false" src="" alt="Imagem da palavra secreta">
+      </div>
+    </div>
+    <div class="row">
+      <div class="TimeDisplay">
+        <div class="row">
+          <div class="Time-column" style="align-content:flex-end">
+            <div class="Time-Box">
+              <h5 id="minuto-text">MINUTOS</h5>
+              <h1 id="minuto">02</h1>
+            </div>
+          </div>
+          <div class="Time-column">
+            <div class="Time-Box">
+              <h5 id="segundo-text">SEGUNDOS</h5>
+              <h1 id="segundo">30</h1>
+            </div>
+          </div>
+          <div class="Time-column" style="align-content:flex-start">
+            <div class=" Time-Box">
+              <h5 id="milisegundo-text">MILISEGUNDOS</h5>
+              <h1 id="milisegundo">99</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-  <!--/CAIXA-->
-  <script src="js\JavaScript.js"></script>
+  <script src="js/JavaScript.js"></script>
   <script src="js/jogo.js"></script>
 </body>
 
