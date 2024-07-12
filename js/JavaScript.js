064a6c742e3bc0f23df2c16b9e29212c2dd9d815
Rúben Domingues Pires <rubendominguespires@gmail.com>
2024-07-12 15:09:54 +0100

    GRANDE ATUALIZAÇÃO (javascript.js)
    ✅ Adicionado - Variáveis e Tempo Inicial:
    Foram adicionadas variáveis para a imagem GIF, texto interno das caixas de minutos, segundos e milissegundos, além das variáveis para o modo de dificuldade. O tempo inicial do jogo foi predefinido como dificuldade fácil, com 2 minutos, 30 segundos e 99 milissegundos. Isso foi configurado para iniciar o site no modo fácil com esse tempo.
    
    ❌ Removido - Função de Refresh ao Reiniciar:
    Removi uma função que causava o refresh da página quando o usuário clicava nos botões "Reiniciar" ou "Recomeçar". Isso estava conflitando com outras funções, especialmente com o modo escuro/claro, pois reiniciava a página e retornava ao tema padrão (modo escuro) mesmo se o usuário estivesse jogando com o tema claro.
    
    ✅ Adicionado - Funções de Ativar e Desativar Input:
    Foram adicionadas duas funções para ativar e desativar o input do formulário, permitindo que o usuário insira a palavra. Essas funções são chamadas "desativarCampoDeEntrada" e "ativarCampoDeEntrada".
    
    ✅ Adicionado - Adaptação do Texto de Tempo ao Tamanho da Tela:
    Foi adicionada uma função que ajusta o texto das caixas de tempo ("minuto", "segundo" e "milissegundo") com base no tamanho da tela. Quando a tela for menor que 910 pixels, o texto será alterado para "Min", "Seg" e "MS". Quando menor que 430 pixels, será ajustado para "M", "S" e "MS". Isso evita que o texto saia das caixas em telas menores.
    
    ✅ Adicionado (Hamburguer) - Melhoria no Ícone do Menu:
    Implementei o código necessário para transformar o ícone do "hamburguer" em um ícone funcional. Ao clicar no ícone, ele adiciona a classe "active" para estilização adicional ao botão e também ativa o menu off-screen.
    
    ✅ Adicionado (Tema) - Implementação do Tema Escuro:
    Foram adicionadas as funções necessárias para suportar o tema escuro. Ao clicar no ícone da lua, o tema escuro é ativado. A lua é substituída pelo ícone de sol do Bootstrap e a classe "dark-mode" é adicionada ao <html>. No CSS, foram definidas variáveis para mudar automaticamente as cores do sistema conforme o tema escuro.
    
    ✅ Adicionado (Blur) - Aplicação Inicial de Blur na Imagem:
    Foi adicionado um efeito de blur inicial de 3px na imagem usada como dica, devido ao tema escuro e à dificuldade inicial configurada como fácil. Isso é aplicado quando o jogo inicia, mostrando o formulário, o temporizador e a imagem com esse efeito.

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,70 +1,138 @@
 var botao = document.getElementById("PlayButton");
 var formulario = document.getElementById("Formulario");
 var timeDisplay = document.querySelector(".TimeDisplay");
+var imageDisplay = document.querySelector(".Image-row");
 
 //------------------------------------------------------------------
 
-var minuto = document.getElementById("minuto");
-var segundo = document.getElementById("segundo");
-var milisegundo = document.getElementById("milisegundo");
+var minuto_text = document.getElementById("minuto-text");
+var segundo_text = document.getElementById("segundo-text");
+var milisegundo_text = document.getElementById("milisegundo-text");
 
 //------------------------------------------------------------------
 
-botao.addEventListener("click", function () {
-  formulario.style.display = "initial";
-  setTimeout(function () {
-    timeDisplay.style.display = "flex";
-  }, 1100);
+var imagem = document.getElementById("imagem");
 
-  setTimeout(() => {
-    botao.innerText = "REINICIAR";
-    let minuto = 2;
-    let segundo = 30;
-    let milisegundo = 99;
+//------------------------------------------------------------------
+//------------------------DIFICULDADE-------------------------------
 
-    const timer = setInterval(() => {
+var facil = document.getElementById("botao-facil");
+var medio = document.getElementById("botao-medio");
+var dificil = document.getElementById("botao-dificil");
+var dificuldade = "Fácil";
 
-      milisegundo--;
+var minuto = 2;
+var segundo = 30;
+var milisegundo = 99;
+//------------------------------------------------------------------
 
-      if (milisegundo < 0) {
-        milisegundo = 99;
-        segundo--;
-        if (segundo < 0) {
-          segundo = 59;
-          minuto--;
-          if (minuto < 0) {
-            clearInterval(timer); //TERMINAR O TIMER
-            botao.innerText = "RECOMEÇAR";
+function ativarCampoDeEntrada() {
+  formulario.disabled = false;
+}
 
-            minuto = 0;
-            segundo = 0;
-            milisegundo = 0;
+function desativarCampoDeEntrada() {
+  formulario.disabled = true;
+}
 
-            document.getElementById("minuto").innerText = "00";
-            document.getElementById("segundo").innerText = "00";
-            document.getElementById("milisegundo").innerText = "00";
+function mudarTextoComEcra() {
+
+  var ecra = window.innerWidth;
+
+  if (ecra <= 910) {
+    minuto_text.innerText = 'Min';
+    segundo_text.innerText = 'Seg';
+    milisegundo_text.innerText = 'MS';
+
+    if (ecra <= 430) {
+      minuto_text.innerText = 'M';
+      segundo_text.innerText = 'S';
+      milisegundo_text.innerText = 'MS';
+    }
+  } else {
+    minuto_text.innerText = 'Minutos';
+    segundo_text.innerText = 'Segundos';
+    milisegundo_text.innerText = 'Milissegundos';
+  }
+}
+mudarTextoComEcra();
+window.addEventListener('resize', mudarTextoComEcra);
+
+const hamMenu = document.querySelector(".ham-menu");
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+
+// THEME CODE
+const $html = document.querySelector("html");
+const toggle = document.getElementById("toggleLight");
+
+toggle.addEventListener("click", function () {
+  this.classList.toggle("bi-sun");
+  $html.classList.toggle("dark-mode");
+})
+
+let timer;
+
+function Tempo() {
+  botao.addEventListener("click", function () {
+    imagem.style.filter = 'blur(3px)';
+    formulario.style.display = "initial";
+    setTimeout(function () {
+      timeDisplay.style.display = "flex";
+    }, 1100);
+    setTimeout(function () {
+      imageDisplay.style.display = "flex";
+    }, 1800);
+
+    setTimeout(() => {
+      botao.innerText = "REINICIAR";
+      if (timer) {
+        clearInterval(timer);
+      }
+
+      timer = setInterval(() => {
+        milisegundo--;
+
+        if (milisegundo < 0) {
+          milisegundo = 99;
+          segundo--;
+          if (segundo < 0) {
+            segundo = 59;
+            minuto--;
+            if (minuto < 0) {
+              clearInterval(timer); // Termina o timer
+              botao.innerText = "RECOMEÇAR";
+
+              if (botao.innerText === "RECOMEÇAR") {
+                desativarCampoDeEntrada();
+                imagem.style.filter = 'blur(0px)';
+              }
+
+              minuto = 0;
+              segundo = 0;
+              milisegundo = 0;
+
+              document.getElementById("minuto").innerText = "00";
+              document.getElementById("segundo").innerText = "00";
+              document.getElementById("milisegundo").innerText = "00";
+            }
           }
         }
-      }
 
-      const minutoFormatado = minuto < 10 ? '0' + minuto : minuto; //adicionado um zero à esquerda, caso contrário,
-      const segundoFormatado = segundo < 10 ? '0' + segundo : segundo;// o valor do minuto é mantido. Isso garante que o
-      const milisegundoFormatado = milisegundo < 10 ? '0' + milisegundo : milisegundo;//minuto seja exibido sempre com dois dígitos.
+        const minutoFormatado = minuto < 10 ? '0' + minuto : minuto;
+        const segundoFormatado = segundo < 10 ? '0' + segundo : segundo;
+        const milisegundoFormatado = milisegundo < 10 ? '0' + milisegundo : milisegundo;
 
-      document.getElementById("minuto").innerText = minutoFormatado;
-      document.getElementById("segundo").innerText = segundoFormatado;
-      document.getElementById("milisegundo").innerText = milisegundoFormatado;
+        document.getElementById("minuto").innerText = minutoFormatado;
+        document.getElementById("segundo").innerText = segundoFormatado;
+        document.getElementById("milisegundo").innerText = milisegundoFormatado;
 
-    }, 10); //REFRESH A CADA 10 SEGUNDOS
+      }, 10); // Atualiza a cada 10 milissegundos
 
-
-    botao.addEventListener("click", function () {
-      if (botao.innerText === "REINICIAR" || botao.innerText === "RECOMEÇAR") {
-        location.reload(); // Refresh da pagina
-      }
-    });
-
-  }, 2000);
-
-
-});
+    }, 2000);
+  });
+}
+Tempo();
